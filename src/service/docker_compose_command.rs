b72672901a66ec59a3bdb053ab0304dee8c11ad1
mgkang <mgkang@babkorea.com>
2024-10-04 17:41:44 +0900

    원복

--- src/service/docker_compose_command.rs
+++ src/service/docker_compose_command.rs
@@ -19,8 +19,6 @@ impl DockerComposeCommandTrait for DockerComposeCommand {
             .args(args)
             .output()?;
 
-        println!("{:?}", output);
-
         if output.status.success() {
             Ok(String::from_utf8_lossy(&output.stdout).to_string())
         } else {
@@ -31,45 +29,28 @@ impl DockerComposeCommandTrait for DockerComposeCommand {
         }
     }
 
-    fn compose_down (current_dir: &PathBuf) {
+    fn compose_down (current_dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
         let args = Args::parse();
         let file_name = args.file.as_str();
-        
-        let output = Command::new("docker-compose")
-        .arg("-f")
-        .arg(current_dir.join(file_name).to_str().unwrap())
-        .arg("down")
-        .arg("--rmi")
-        .arg("all")
-        .output()
-        .expect("Failed to execute docker-compose command");
-
-        if output.status.success() {
-            println!("Docker Compose command executed successfully");
-            println!("Output: {}", String::from_utf8_lossy(&output.stdout));
-        } else {
-            eprintln!("Docker Compose command failed");
-            eprintln!("Error: {}", String::from_utf8_lossy(&output.stderr));
-        }
+        DockerComposeCommand::run(&[
+            KeyOption::F,
+            current_dir.join(file_name).to_str().unwrap(),
+            KeyCommand::DOWN,
+            KeyDBOption::RMI,
+            KeyCommand::ALL
+        ])?;
+        Ok(())
     }
 
-    fn compose_up (current_dir: &PathBuf) {
+    fn compose_up (current_dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
         let args = Args::parse();
         let file_name = args.file.as_str();
-        let output = Command::new("docker-compose")
-        .arg("-f")
-        .arg(current_dir.join(file_name).to_str().unwrap())
-        .arg("up")
-        .arg("-d")
-        .output()
-        .expect("Failed to execute docker-compose command");
-
-        if output.status.success() {
-            println!("Docker Compose command executed successfully");
-            println!("Output: {}", String::from_utf8_lossy(&output.stdout));
-        } else {
-            eprintln!("Docker Compose command failed");
-            eprintln!("Error: {}", String::from_utf8_lossy(&output.stderr));
-        }
+        DockerComposeCommand::run(&[
+            KeyOption::F,
+            current_dir.join(file_name).to_str().unwrap(),
+            KeyCommand::UP,
+            KeyOption::D
+        ])?;
+        Ok(())
     }
 }
