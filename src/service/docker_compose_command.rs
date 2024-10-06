3a2858a135b4727803b7e658059a06d3ea0d36c4
m1n67un <rkdalsrbs0@gmail.com>
2024-10-06 20:52:22 +0900

    update

--- src/service/docker_compose_command.rs
+++ src/service/docker_compose_command.rs
@@ -1,6 +1,9 @@
 use std::{
     path::PathBuf, 
-    process::Command
+    process::{
+        Command,
+        Stdio
+    }
 };
 use clap::Parser;
 
@@ -12,11 +15,18 @@ use crate::{
         args::Args, key_command::KeyCommand, key_db_option::KeyDBOption, key_option::KeyOption
     }
 };
+use std::io::{
+    BufRead,
+    BufReader
+};
+
 
 impl DockerComposeCommandTrait for DockerComposeCommand {
     fn run(args: &[&str]) -> Result<String, std::io::Error> {
         let output = Command::new(KeyCommand::DOCKER_COMPOSE)
             .args(args)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .output()?;
 
         if output.status.success() {
@@ -45,12 +55,33 @@ impl DockerComposeCommandTrait for DockerComposeCommand {
     fn compose_up (current_dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
         let args = Args::parse();
         let file_name = args.file.as_str();
-        DockerComposeCommand::run(&[
-            KeyOption::F,
-            current_dir.join(file_name).to_str().unwrap(),
-            KeyCommand::UP,
-            KeyOption::D
-        ])?;
+        let mut child = Command::new("docker-compose")
+                            .args(&[
+                                KeyOption::F, 
+                                current_dir.join(file_name).to_str().unwrap(), 
+                                KeyCommand::UP, 
+                                KeyOption::D
+                            ])
+                            .stdout(Stdio::piped())
+                            .stderr(Stdio::piped())
+                            .spawn()?;
+
+        let stdout = child.stdout.take().unwrap();
+        let stderr = child.stderr.take().unwrap();
+
+        let stdout_reader = BufReader::new(stdout);
+        let stderr_reader = BufReader::new(stderr);
+
+        for line in stdout_reader.lines() {
+            println!("stdout: {}", line?);
+        }
+
+        for line in stderr_reader.lines() {
+            println!("stderr: {}", line?);
+        }
+
+        let status = child.wait()?;
+        println!("Exited with status: {}", status);
         Ok(())
     }
 }
